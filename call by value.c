5088c77825c33a602fdf61b94f9f07666a184b67
DesuRamya <119840662+DesuRamya@users.noreply.github.com>
2023-05-02 08:30:59 +0530

    Add files via upload

---
+++ call by value.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void modify(int a)
+{
+	a=a+1;
+printf("%d\n",&a);	
+}
+int main()
+{
+	int a=10;
+	printf("%d\n",&a);
+//	printf("before:%d\n",a);
+	modify(a);
+//	printf("after:%d\n",a);
+}
