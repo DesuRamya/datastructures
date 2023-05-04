5e024a1131f7af717a3e0bfb03512b299885b55f
DesuRamya <119840662+DesuRamya@users.noreply.github.com>
2023-05-04 08:44:31 +0530

    Add files via upload

---
+++ merge_sort_recursion.c
@@ -0,0 +1,48 @@
+void merge(int *arr,int l,int h,int m)
+{
+	int i=l,j=m+1,k=0,res[h-l+1];
+	while(i<=m && j<=h)
+	{
+		if(arr[i]>arr[j])
+		res[k++]=arr[j++];
+		else
+		res[k++]=arr[i++];
+	}
+		while(j<=h)
+		{
+			res[k++]=arr[j++];
+		}
+			while(i<=m)
+		{
+			res[k++]=arr[i++];
+		}
+		k=0;
+for(i=l;i<=h;i++)
+arr[i]=res[k++];
+}
+#include<stdio.h>
+void merge_sort(int *arr,int l,int h)
+{
+	if(l<h)
+	{
+		int m;
+		m=(l+h)/2;
+		merge_sort(arr,l,m);
+		merge_sort(arr,m+1,h);
+		merge(arr,l,h,m);
+	}
+}
+int main()
+{
+	int arr[100],i,n;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	merge_sort(arr,0,n-1);
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",arr[i]);
+	}
+}
